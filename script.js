d776dd10e1d5fd8f55f47c2596ddb7938ece998e
Shafawra <shafaaura35@gmail.com>
2025-09-03 14:41:16 +0800

    commit

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ btnTheme.addEventListener("click", () => {
     document.body.style.background = "#222";
     document.body.style.color = "whitesmoke"
 })
+
+let btnHeader = document.querySelectorAll(".btnHeader");
+let icon = document.querySelectorAll(".icon");
+let desc = document.querySelectorAll(".faq-content");
+
+btnHeader.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        desc[i].classList.toggle("max-h-0");
+        disc[i].classList.toggle("max-h-fit");
+    });
+})
+
+
+
